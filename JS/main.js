f7ccb2e0365024c3d3a1e17c2da22bec9e9d15dd
yang052413 <liyang0525.ly@gmail.com>
2020-03-10 17:37:59 -0700

    realtime trending,all users,hashtag done

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     //初始化firebase数据库
     var db = firebase.firestore();
     var storageRef = firebase.storage().ref();
-
+    var tag = [];
     //用户上传照片功能
     // firebase.auth().onAuthStateChanged(function (user) {
     //     $("#upload-image").click(function () {
@@ -27,7 +27,6 @@ $(document).ready(function () {
     //             })
     //     });
     // });
-
 
     //用户发送信息时间 Get the current time
     var today = new Date();
@@ -84,6 +83,15 @@ $(document).ready(function () {
                 var tweet_usernickname = change.doc.data().Nickname;
                 var tweet_userProfile = change.doc.data().Profile;
 
+                var tweet_tag;
+                var tweet_tag_wrap = $("<div class=tweet-tag-wrap></div>");
+                if (change.doc.data().Tag != null) {
+                    for (var i = 0; i < change.doc.data().Tag.length; i++) {
+                        tweet_tag = $("<button class=tweet-tag>" + change.doc.data().Tag[i] + "</button>");
+                        tweet_tag_wrap.append(tweet_tag);
+                    }
+                }
+
                 if (tweet_image == null) {
                     var user_post = $("<div class=user-post>" +
                         "<div class=user-post-wrap>" +
@@ -95,10 +103,10 @@ $(document).ready(function () {
                         "<span class=user-post-time>" + tweet_date + "</span>" +
                         "</p>" +
                         "<p class=current-user-post>" + tweet_content + "</p>" +
-                        "</div>" +
-                        "</div>" +
-                        "<div class=heart></div>"
+                        "</div>"
                     );
+                    user_post.append(tweet_tag_wrap);
+
                 } else {
                     var user_post = $("<div class=user-post>" +
                         "<div class=user-post-wrap>" +
@@ -110,13 +118,14 @@ $(document).ready(function () {
                         "<span class=user-post-time>" + tweet_date + "</span>" +
                         "</p>" +
                         "<p class=current-user-post>" + tweet_content + "</p>" +
+                        "<div class=tweet-tag-wrap></div>" +
                         "</div>" +
                         "</div>" +
                         "<div class=user-img-wrap>" +
                         "<img class=user-post-img src=" + tweet_image + ">" +
-                        "</div>" +
-                        "<div class=heart></div>"
+                        "</div>"
                     );
+                    user_post.append(tweet_tag_wrap);
                 }
 
                 //加入新的推文 
@@ -129,6 +138,23 @@ $(document).ready(function () {
 
     firebase.auth().onAuthStateChanged(function (user) {
         //发布新的推文 信息写入到云端
+        $("#tag-input").keyup(function (event) {
+            var tagMsg = $("#tag-input").val();
+
+            if (event.keyCode == 13 && tagMsg != '') {
+                tag.push(tagMsg);
+                $(".tag-placeholder").append("<button class=tweet-tag>" + tagMsg +"</div>");
+                $('input[type="text"]').val('');
+
+                var tagDoc = {
+                    TagName: tagMsg,
+                };
+
+                db.collection("tag").add(tagDoc);
+                console.log(tag);
+            }
+        });
+
         $("#post-btn").click(function () {
             db.collection("user").doc(user.uid).onSnapshot(function (snap) {
                 //用户推文数据
@@ -136,19 +162,19 @@ $(document).ready(function () {
                 var tweetContent = $("#user-input").val();
                 var file = $("#image-input").prop('files')[0];
 
-                if(snap.data().name == null) {
+                if (snap.data().name == null) {
                     user_name = user.displayName;
                 } else {
                     user_name = snap.data().name;
                 }
 
-                if(snap.data().nickname == null) {
+                if (snap.data().nickname == null) {
                     user_nickname = "コート上の王様";
                 } else {
                     user_nickname = snap.data().nickname;
                 }
 
-                if(snap.data().profile == null) {
+                if (snap.data().profile == null) {
                     user_profile = "Resource/user.jpg";
                 } else {
                     user_profile = snap.data().profile;
@@ -170,7 +196,7 @@ $(document).ready(function () {
                     //写入tweet内容到firebase
                     task.then(snapshot => snapshot.ref.getDownloadURL())
                         .then(url => {
-                            console.log("图像上传成功 链接为: " + url);;
+                            console.log("图像上传成功 链接为: " + url);
                             var tweetData = {
                                 Content: tweetContent,
                                 Date: dateTime,
@@ -178,12 +204,13 @@ $(document).ready(function () {
                                 Name: user_name,
                                 Nickname: user_nickname,
                                 Profile: user_profile,
+                                Tag: tag,
                             };
                             db.collection("tweet").add(tweetData);
-
+                            tag = [];
                         })
-
                     $('input[type="text"], input[type="file"], textarea').val('');
+
                     //用户选择只上传信息 没有图片
                 } else if (file == null && tweetContent != '') {
                     var tweetData = {
@@ -192,11 +219,15 @@ $(document).ready(function () {
                         Name: user_name,
                         Nickname: user_nickname,
                         Profile: user_profile,
+                        Tag: tag,
                     };
                     db.collection("tweet").add(tweetData);
+                    tag = [];
                     $('input[type="text"], input[type="file"], textarea').val('');
-                } 
+                    $(".tag-placeholder").empty();
+                }
             });
+            console.log(tag);
         });
 
 
@@ -261,11 +292,6 @@ $(document).ready(function () {
         });
     });
 
-
-
-
-
-
     $(".nav-bar-item-wrap").mouseenter(function () {
         $(this).css({
             "color": "rgb(29, 161, 242)",
@@ -300,6 +326,71 @@ $(document).ready(function () {
     $(document).on("animationend", ".heart", function () {
         $(this).toggleClass('is_animating');
         console.log("点击❤️");
+    });
+
+
+    //所有用户的信息
+    db.collection("user").get().then(function (snap) {
+        snap.forEach(function (doc) {
+            let recommend_name = doc.data().name;
+            let recommend_nickname = doc.data().nickname;
+            let recommend_profile = doc.data().profile;
+
+            let recommend_item = $("<div class=recommend-item>" +
+                "<img class=recommend-user src=" + recommend_profile + " width=auto height=100px>" +
+                "<div class=recommend-text-wrap>" +
+                "<p class=recommend-item-name>" + recommend_name + "</p>" +
+                "<p class=recommend-item-at>@" + recommend_nickname + "</p>" +
+                "</div>" +
+                "<button class=user-subscribe-btn>Follow</button>" +
+                "</div>");
+
+            $(".recommendation-block").append(recommend_item);
+        });
+    });
+    
+    var tagStat = [];
+    db.collection("tag").get().then(function (snap) {
+        snap.forEach(function (doc) {
+            let tag_name = doc.data().TagName;
+            tagStat.push(tag_name);
+        });
+
+        tagStat.sort();
+
+        var current = null;
+        var cnt = 0;
+        for (var i = 0; i <= tagStat.length; i++) {
+            if (tagStat[i] != current) {
+                if (cnt > 0) {
+                    console.log(current + ' 显示了' + cnt + '次');
+                    let trend_block_item = $("<div class=trend-block-item>" +
+                        "<p class=trend-block-where>Trending in Canada</p>" +
+                        "<p class=trend-block-tag>#" + current + "</p>" +
+                        "<p class=trend-block-post>" + cnt + " tweets</p>" +
+                        "</div>");
+
+                    $(".trend-block").append(trend_block_item);
+                }
+                current = tagStat[i];
+
+                cnt = 1;
+                //如果相等 递增
+            } else {
+                cnt++;
+            }
+        }
+    });
+
+    //退出当前用户 Sign out the user from firebase
+    $("#logout-btn").click(function () {
+        firebase.auth().signOut().then(function () {
+            // Sign-out successful. Open up the login page
+            window.location.replace("index.html");
+        }).catch(function (error) {
+            console.log("Erros...during signout");
+        });
+
     });
 
 });
